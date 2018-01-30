b48e76ff95041bbea7cc3797837020f9dde80bc3
Ganesh <marutiganesh1996@gmail.com>
2018-01-30 12:32:53 +0530

    Dictionary for conversion process

---
+++ dict.py
@@ -0,0 +1,51 @@
+Relational=  {'*':'multiplied by','-':'minus','div':'divided by','+':'plus','times':'multiplied to','<':'is less than','nless':'is not less than','leq':'is less than or equal to','leqslant':'is less than or equal to',
+             'nleq':'is neither less than nor equal to','nleqslant':'is neither less than nor equal to','prec':'precedes',
+             'nprec':'does not precede','preceq':'precedes or equals','npreceq':'neither precedes nor equals','\ll':'###','\lll':'###',
+             'subset':'is a proper subset of','\not\subset':'is not a proper subset of','subseteq':'is a subset of',
+             'nsubseteq':'is not a subset of','sqsubset':' ','sqsubset eq':'','>':'is greater than','ngtr':'is not greater than',
+             'geq':'is greater than or equal to','geqslant':'is greater than or equal to','ngeq':'is neither greater than nor equal to',
+             'succ':'succeeds','nsucc':'does not succeed','succeq':'succeeds or equals','nsucceq':'neither succeeds nor equals',
+             'gg':'###','ggg':'###','supset':'is a proper superset of','\not\supset':'is not a proper superset of','supseteq':'is a superset of',
+             'nAsupseteq':'is  not a superset of','squpset':'###','squpseteq':'###',
+             'parallel':'is parallel with','nparallel':'is not parallel with','asymp':'is asymptotic to','bowtie':'###','vadash':'###',
+             'dashv':'###','in':'is member of','ni':'owns,has member','smile':'###','frowm':'###','models':'models','notin':'is not a member of',
+             'perp':'is perpendicular with','mid':'divides',
+             '=':'is equal to','doteq':'###','equiv':'is equivalent to','\approx':'is approximately','\cong':'is congruent to',
+             'simeq':'is similar or equal to','\sim':'is similar to','\propto':'is proportional to','\neq':'is ot equal to',
+             'ne':'is not equal to','^':'to the superscript of',
+              #Geometry
+               '\overline{\rm AB}':'segment','\angle':'angle','\triangle':'triange','\cong':'congruent','\sim':'similar','\l':'is parallel with',
+              '\perp':'is perpendicular to','\overrightarrow{\rm AB}':'ray','\measuredangle':'measured angle','\square':'square','\ncong':'not congruent',
+              '\nsim':'not similar','\nparallel':'is not parallel with','\not\prep':'is not perpendicular to',
+              #Trigonometric
+                #Binary
+                'pm':'plus or minus','mp':'minus or plus','times':'multiplied to','div':'divided by','ast':'###','star':'###','circ':'composed with','bullet':'dot or bullet product',
+              'cdot':'cross product of','cap':'intersected with','cup':'union of','upplus':'###','sqcap':'###','sqcup':'###','vee':'###','wedge':'Kulkarni–Nomizu product','setminus':'without',
+              'wr':'wreath product of','diamond':'###','bigtriangleup':'###','bigtriangledown':'###','triangleleft':'is a normal subgroup of','triangleright':'is a normal subgroup of','lhd':'###',
+              'rhd':'###','unlhd':'###','unrhd':'###','oplus':'xor','ominus':'symmetric difference','otimes':'tensor product of','oslash':'###','odot':'###','bigcirc':'###','dagger':'conjugate transpose',
+              '\ddagger':'###','amalg':'###','+':'plus','-':'minus',
+              #Delimeter
+              'vert':'vertical line','|':'double vertical lines','Vert':'double vertical lines','langle':'left angle bracket','rangle':'right angle bracket',
+              'lfloor':'left floor function','rfloor':'right floor function','lceil':'left ceil function','rceil':'right ceil function','/':'forwardslash','backslash':'backslash','[':'left square bracket',
+              ']':'right square bracket','uparrow':'uparrow','downarrow':'down arrow','Uparrow':'double uparrow','Downarrow':'double downarrow','llcorner':'lower left corner','lrcorner':'lower right corner',
+              'ulcorner':'upper left corner','urcorner':'upper right corner',
+               #Arrows
+              'rightarrow':'rightarrow or to','mapsto':'maps to','leftarrow':'leftarrow or gets','\Rightarrow':'Rightarrow','\Leftarrow':'Leftarrow','\longrightarrow':'longrightarrow or to',
+             'longmapsto':'long maps to','longleftarrow':'longleftarrow','Longrightarrow':'Longrightarrow','Longleftarrow':'Longleftarrow','uparrow':'uparrow','\downarrow':'downarrow',
+             'updownarrow':'updownarrow','Uparrow':'Uparrow','Downarrow':'Downarrow','Updownarrow':'Updownarrow'}
+greekletters ={'varepsilon':'epsilon','vartheta':'theta','varkappa':'kappa','varpi':'pi','rho':'rho','varphi':'phi','curlyclose':'close curly brace','curlyopen':'open curly brace',
+               '(':'open parenthesis',')':'close parenthesis',
+#trignomentry
+               'sin':'sin','cos':'cos','tan':'tan','arcsin':'arcsin','arccos':'arccos','arctan':'arctan','csc':'cosecon','sec':'secon',
+               'cot':'cot','arccsc':'arccosecon','arccot':'arccot','sinh':'hyperbolic sin fuction of','cosh':'hyperbolic cos function  of','tanh':'hyperbolic tan fuction of',
+               'arcsinh':'hyperbolic arcsin function of','arccosh':'hyperbolic arccos function of','arctanh':'hyperbolic arctan function of',
+               'csch':'hyperbolic cosecon function of','sech':'hyperbolic secon function of','coth':'hyperbolic cot functon of',
+               'arcsch':'hyperbolic arccosecon function of','arcsech':'hyperbolic arcsecon function of','arcoth':'hyperbolic arccot function of',
+               '^': 'to the superscript of','sum_': 'sumation from', 'prod_': 'product from ','int_':'integral of limits from','to':'tense to','infty':'infinity','lim_':'limit'           }
+Negationdbin = {'neq': 'is not equal to', 'ne': 'is not equal to', 'nless': 'is not less than','nleq': 'is not less than or equal to','nnprec': 'does not precede',
+                'npreceq': 'neither precedes nor equals', 'nsim': 'is not similar to', 'notin': 'is not member of','ngtr': 'is not greater than',
+                'ngeq': 'is not greater than or equal to','nsucc': 'does not succeed', 'nsucceq': 'neither succedes nor equals',
+                'ncong': 'is not congruent','nparallel': 'is not parallel with'}
+Braces=      {'(': 'openparanthasis', ')': 'closeparanthesis','curlyclose':'close curly brace','curlyopen':'open curly brace','(':'open parenthesis',')':'close parenthesis'}
+
+#rhs={'sum_': 'sumation from', 'prod_': 'product from ',}
