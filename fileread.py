7f846ecc0f2231197a10605eaffc7e9237bd3d06
Ganesh <marutiganesh1996@gmail.com>
2018-01-30 12:31:43 +0530

    reads a file and provides input to speak module

---
+++ fileread.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import webbrowser as wb
+import speak
+def rf(str):
+    try:
+        print("Building text to speech system")
+
+        print("Opening and closing a file.")
+        text_file = open("G:/LaTex/TeXmakerprj/try.pdf", "r")
+        text_file.close()
+
+        text_file = open("G:/LaTex/TeXmakerprj/try.pdf", "r")
+        lines = text_file.readlines()
+        for line in lines:
+            lang = 'en'
+            speak.tts(line, lang)
+            text_file.close()
+    except Exception as e:
+        print(e)
+
