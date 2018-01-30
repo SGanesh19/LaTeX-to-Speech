e8482dbefdacce53df1dcd6ac15e4d3c8a87c254
Ganesh <marutiganesh1996@gmail.com>
2018-01-30 11:12:33 +0530

    This is for getting a equation from tex document

--- getEquation.py
+++ getEquation.py
@@ -12,32 +12,27 @@ def main():
         Inputstr = " "
         intermediateResult = " "
         matchingTags = r"begin{equation}(.*?)end{equation}"
+
         print("Opening a file...")
-        # Inputfile = open("G:/LaTex/ALL TRYS/Newtry1/S/Inputdoc.tex", "r")
-        # Inputfile.close()
         Inputfile = open("G:/LaTex/ALLTRYS/Newtry1/S/Inputdoc.tex", "r")
         Inputstr = Inputfile.read()
         print("Reading done")
-        # path ="G:/LaTex/ALLTRYS/Newtry1/S/Inputdoc.tex"
-        # Inputstr = rf(path)
         matches = re.finditer(matchingTags, Inputstr, re.MULTILINE | re.DOTALL)
         print("Matching string")
-        # while (intermediateResult == 0):
         for matchNum, match in enumerate(matches):
             for groupNum in range(0, len(match.groups())):
                 intermediateResult = match.group(1)
-                print(intermediateResult)
         result = convert(intermediateResult)
-        # print(result)
         Inputfile.close()
         print("terminated reading mode")
-        # print(result)
+
         print("Writing file")
         with open("G:/LaTex/ALLTRYS/Newtry1/S/Outputdoc.txt", "w") as Outputfile:
             Outputfile.write(result)
         print("completed writing")
         Outputfile.close()
         print("Process completed")
+
     except:
         print("error")
 
