50606e969f77a95cc52893369b53bcadc63cc5ba
Ganesh <marutiganesh1996@gmail.com>
2018-02-02 16:26:17 +0530

    reads the file using gTTS

--- Speak.py
+++ Speak.py
@@ -1,14 +1,15 @@
 from gtts import gTTS
 import pyglet
 pyglet.lib.load_library('avbin')
-pyglet.have_avbin=True
-import time,os
+pyglet.have_avbin = True
+import time, os
 
-def tts(text, lang):
-    filename = 'G:/LaTex/ALLTRYS/Newtry1/temp/temp.mp3'
+def tts(text,lang):
+    file = gTTS(text = text, lang = 'en')
+    filename = 'D:/temp/temp.mp3'
     file.save(filename)
 
-    music = pyglet.media.load(filename, streaming = False)
+    music = pyglet.media.load(filename, streaming=False)
     music.play()
 
     time.sleep(music.duration)
