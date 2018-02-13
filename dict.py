801e22e4d78e5291a090a680ebd15e311a0f96ed
Pkalaivani <kalaisri1197@gmail.com>
2018-02-13 08:04:27 +0530

    dictionary

---
+++ dict.py
@@ -0,0 +1,59 @@
+Dictionary= {'sqrt': 'square root of','<':'is less than','nless':'is not less than',
+'leq':'is less than or equal to','leqslant':'is less than or equal to','nleq':'is neither less than nor equal to',
+'nleqslant':'is neither less than nor equal to','prec':'precedes','nprec':'does not precede','preceq':'precedes or equals',
+'npreceq':'neither precedes nor equals','subset':'is a proper subset of','\not\subset':'is not a proper subset of',
+'subseteq':'is a subset of','nsubseteq':'is not a subset of','sqsubset':' ','sqsubset eq':'','>':'is greater than',
+'ngtr':'is not greater than','geq':'is greater than or equal to','geqslant':'is greater than or equal to',
+'ngeq':'is neither greater than nor equal to','succ':'succeeds','nsucc':'does not succeed','succeq':'succeeds or equals',
+'nsucceq':'neither succeeds nor equals','supset':'is a proper superset of','\not\supset':'is not a proper superset of',
+'supseteq':'is a superset of','nAsupseteq':'is  not a superset of','parallel':'is parallel with','nparallel':'is not parallel with',
+'asymp':'is asymptotic to','in':'is member of','ni':'owns,has member','models':'models','notin':'is not a member of',
+'mid':'divides','mod': 'modulus of','equiv':'is equivalent to','\approx':'is approximately','curlyopen':'open curly brace',
+'simeq':'is similar or equal to','\sim':'is similar to','\neq':'is ot equal to','ne':'is not equal to','curlyclose':'close curly brace',
+'+':'plus','-':'minus','*':'multiplied by','/':'divided by','=':'is equal to','(':'open parenthesis',')':'close parenthesis',
+#DOTS
+'dots': 'and so on','dotsi': 'dots with integral','\dotsc': 'dots with commas','\dotsb': 'dots with binary operators/relations',
+'dotsm': 'multiplication dots','dot':'product of',
+#BINARY
+'pm':'plus or minus','mp':'minus or plus','times':'multiplied by','div':'divided by','star':'star','circ':'composed with',
+'bullet': 'bullet product','cdot':'cross product of','wedge':'Kulkarni–Nomizu product',
+'setminus':'without','wr':'wreath product of','diamond':'diamond','triangleleft':'is a normal subgroup of','triangleright':
+'is a normal subgroup of','oplus':'xor','ominus':'symmetric difference','otimes':'tensor product of',
+'dagger':'conjugate transpose',
+#DELIMITARS
+'vert':'vertical line','|':'double vertical lines','Vert':'double vertical lines','langle':'left angle bracket',
+'rangle':'right angle bracket','lfloor':'left floor function','rfloor':'right floor function','lceil':'left ceil function',
+'rceil':'right ceil function','backslash':'backslash','llcorner':'lower left corner','lrcorner':'lower right corner',
+'ulcorner':'upper left corner','urcorner':'upper right corner',
+#ARROWS
+'rightarrow':'rightarrow','mapsto':'maps to','leftarrow':'leftarrow or gets','Rightarrow':'Rightarrow','Leftarrow':'Leftarrow',
+'longrightarrow':'longrightarrow or to','longmapsto':'long maps to','longleftarrow':'longleftarrow','Longrightarrow':'Longrightarrow',
+'Longleftarrow':'Longleftarrow','uparrow':'uparrow','downarrow':'downarrow','updownarrow':'updownarrow','Uparrow':'Uparrow','Downarrow'
+:'Downarrow','Updownarrow':'Updownarrow',}
+Greekletters ={'alpha': 'alpha','beta': 'beta','gamma': 'gamma','delta': 'delta','epsilon': 'epsilon','varepsilon': 'epsilon',
+'Gamma':'gamma','Delta':'delta','Theta':'theta','vartheta': 'theta','Lambda': 'lambda','zeta': 'zeta','eta': 'eta','theta': 'theta',
+'iota': 'iota','kappa':'kappa','lambda': 'lambda','mu': 'mu','nu': 'nu','omicorn': 'omicorn','pi':'pi','rho':'rho','sigma': 'sigma',
+'tau': 'tau','upsilon': 'upsilon','phi': 'phi','chi': 'chi','psi': 'psi','omega': 'omega',
+#TRIGNOMENTRY
+'sin':'sin','cos':'cos','tan':'tan','arcsin':'arcsin','arccos':'arccos','arctan':'arctan','csc':'cosecon','sec':'secon',
+'cot':'cot','arccsc':'arccosecon','arccot':'arccot','sinh':'hyperbolic sin fuction of','cosh':'hyperbolic cos function  of',
+'tanh':'hyperbolic tan fuction of','arcsinh':'hyperbolic arcsin function of','arccosh':'hyperbolic arccos function of',
+'arctanh':'hyperbolic arctan function of','csch':'hyperbolic cosecon function of','sech':'hyperbolic secon function of',
+'coth':'hyperbolic cot functon of','arcsch':'hyperbolic arccosecon function of','arcsech':'hyperbolic arcsecon function of',}
+Negationdbin ={'neq': 'is not equal to', 'ne': 'is not equal to', 'nless': 'is not less than','nleq': 'is not less than or equal to',
+'nnprec': 'does not precede','npreceq': 'neither precedes nor equals', 'nsim': 'is not similar to', 'notin': 'is not member of',
+'ngtr': 'is not greater than','ngeq':'is not greater than or equal to','nsucc':'does not succeed','nsucceq':
+'neither succedes nor equals','ncong': 'is not congruent','nparallel': 'is not parallel with'}
+Rhs={'sum_': 'sumation from', 'prod_': 'product of limits from','int_':'integral of ','limits':'limits from','infty':'infinity','lim':'limit',
+'coprod_':'coproduct of limits from','oint_':'open integral of limits from','cap_':'intersection from','cup_':'union from'}
+Special={'^': 'to the superscript of','_': 'to the subscript of','[':'open square bracket',']':'close square bracket',',': 'comma',
+'(':'open parenthesis',')': 'close parenthesis','+': 'plus','-':'minus','*':'mulitiplied by','/':'divided by','=':'is equal to','!':'factorial'}
+Underlines={ 'hat': 'cap','bar': 'bar','vec': 'vector','overrightarrow': 'rightarrow','overleftarrow': 'leftarrow','overline':
+'overline','underline': 'underline','underbrace': 'underbrace','overbrace': 'over brace','xleftarrow': 'left arrow','xrightarrow':
+'rightarrow','overset': 'overset','underset': 'underset','widehat': 'widehat',
+#GEOMETRY
+'angle': 'angle','measuredangle': 'measurement of angle','bigodot': 'circle','cong': 'congruent','triangle': 'triangle',
+'sim': 'similar','overleftrightarrow': 'line','overline':'overline','parallel': 'is parallel to','perp': 'is perpendicular to',
+'leftarrow': 'Left Arrow','rightarrow': 'Right Arrow','leftrightarrow': 'Left Right Arrow','Leftarrow': 'Left Double Arrow',
+'Rightarrow': 'Right Double Arrow','Leftrightarrow': 'Left Right Double Arrow','propto': 'proportional','equvi': 'Identity',
+'stackrel': 'arc',}
